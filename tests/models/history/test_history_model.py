c0c8691d652b152d9f184f1fdb5d7cdbbf241cad
lucas <lucasrosa223@hotmail.com>
2023-09-21 15:30:42 -0300

    resolvendo req7 teste

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,13 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    response_json = json.loads(HistoryModel.list_as_json())
+    assert "text_to_translate" in response_json[1]
+    assert "translate_from" in response_json[1]
+    assert "translate_to" in response_json[1]
+    assert "Do you love music?" in response_json[1].values()
+    assert "en" in response_json[1].values()
+    assert "pt" in response_json[1].values()
