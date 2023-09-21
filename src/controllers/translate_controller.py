35e23cb69723d324061969bf703f4a2b1d479645
lucas <lucasrosa223@hotmail.com>
2023-09-21 18:36:56 -0300

    arrumando identacao

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,12 +1,8 @@
 from flask import Blueprint, render_template, request
-# from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
-# from models.history_model import HistoryModel
 from deep_translator import GoogleTranslator
 
-
 translate_controller = Blueprint("translate_controller", __name__)
-
 
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
@@ -16,6 +12,7 @@ def index():
     translate_from = "pt"
     translate_to = "en"
     translated = "Tradução"
+
     if request.method == "POST":
         text_to_translate = request.form.get("text-to-translate")
         translate_from = request.form.get("translate-from")
@@ -24,15 +21,15 @@ def index():
             source=translate_from,
             target=translate_to
         ).translate(text_to_translate)
-        return render_template(
-            "index.html",
-            languages=languages,
-            text_to_translate=text_to_translate,
-            translate_from=translate_from,
-            translate_to=translate_to,
-            translated=translated
-        )
-
+        
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated
+    )
 
 # Req. 6
 @translate_controller.route("/reverse", methods=["POST"])
@@ -40,16 +37,17 @@ def reverse():
     languages = LanguageModel.list_dicts()
     text_to_translate = request.form.get("text-to-translate")
     translate_from = request.form.get("translate-from")
-    translate_to = request.form.get("translate_to")
+    translate_to = request.form.get("translate-to")
     translated = GoogleTranslator(
         source=translate_from,
         target=translate_to
     ).translate(text_to_translate)
+    
     return render_template(
-            "index.html",
-            languages=languages,
-            text_to_translate=text_to_translate,
-            translate_from=translate_to,
-            translate_to=translate_from,
-            translated=translated
-        )
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=translated
+    )
