6de37ee285ea8b8aa4a811436c249fd2b5eab89c
lucas <lucasrosa223@hotmail.com>
2023-09-21 16:48:31 -0300

    procurando error re4

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -10,14 +10,14 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    languages_model = LanguageModel.list_dicts()
+    languages = LanguageModel.list_dicts()
     text_to_translate = "O que deseja traduzir"
     translate_from = "en"
     translate_to = "pt"
     translated = "Tradução"
     return render_template(
         "index.html",
-        languages_model=languages_model,
+        languages=languages,
         text_to_translate=text_to_translate,
         translate_from=translate_from,
         translate_to=translate_to,
