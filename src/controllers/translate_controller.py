0904fa0a831c45ee27529cfbdf1d7a2cfac2b496
lucas <lucasrosa223@hotmail.com>
2023-09-21 17:13:15 -0300

    resolvendo 5 e 6 talvez tenha quebrado o 4

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 # from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 # from models.history_model import HistoryModel
+from deep_translator import GoogleTranslator
 
 
 translate_controller = Blueprint("translate_controller", __name__)
@@ -15,17 +16,34 @@ def index():
     translate_from = "en"
     translate_to = "pt"
     translated = "Tradução"
-    return render_template(
-        "index.html",
-        languages=languages,
-        text_to_translate=text_to_translate,
-        translate_from=translate_from,
-        translate_to=translate_to,
-        translated=translated
-    )
+    if request.method == "GET":
+        return render_template(
+            "index.html",
+            languages=languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated
+        )
 
 
 # Req. 6
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse():
-    raise NotImplementedError
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate_to")
+    google_translator = GoogleTranslator(
+        source=translate_from,
+        target=translate_to
+    ).translate(text_to_translate)
+    return render_template(
+            "index.html",
+            languages=languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=google_translator
+        )
+
